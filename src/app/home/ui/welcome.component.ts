9b0f280e402e6acc2053df025f597823ff62e476
Brian Chambers <brianmchambers@proton.me>
2025-02-20 17:01:08 -0600

    enhance WelcomeComponent with dynamic input and template outlets

--- src/app/home/ui/welcome.component.ts
+++ src/app/home/ui/welcome.component.ts
@@ -1,8 +1,36 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NgTemplateOutlet } from "@angular/common";
 
 @Component({
     selector: "app-welcome",
-    template: ` <p>Hi, Josh!</p> `,
+    template: `
+        <input [(ngModel)]="firstName" />
+        <button (click)="handleClick($event)">click me</button>
+        <p>Hi, {{ firstName }}!</p>
+        <p #myParagraph></p>
+        <button (click)="myParagraph.innerHTML = 'Once upon a time...'">Click me</button>
+        <ng-template #myTemplate>
+            <p>Hello there</p>
+        </ng-template>
+        <ng-container *ngTemplateOutlet="myTemplate"></ng-container>
+        <ng-container *ngTemplateOutlet="myTemplate"></ng-container>
+        <ng-container *ngTemplateOutlet="myTemplate"></ng-container>
+        <ng-template #myTemplate2 let-greeting="greeting">
+            <p>{{ greeting }} there</p>
+        </ng-template>
+        <ng-container *ngTemplateOutlet="myTemplate2; context: { greeting: 'Hi' }"></ng-container>
+        <ng-container *ngTemplateOutlet="myTemplate2; context: { greeting: 'Hello' }"></ng-container>
+        <ng-container *ngTemplateOutlet="myTemplate2; context: { greeting: 'Yo' }"></ng-container>
+     `,
+     imports: [FormsModule, NgTemplateOutlet]
 })
 
-export class WelcomeComponent {}
+export class WelcomeComponent {
+    firstName = 'Josh';
+
+    handleClick(ev: MouseEvent) {
+        console.log('I was clicked!');
+        console.log(ev);
+    }
+}
