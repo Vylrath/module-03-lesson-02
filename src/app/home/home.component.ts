40b016587c0d2e083f532d0a5c4a33950f397aff
Brian Chambers <brianmchambers@proton.me>
2025-02-22 10:20:35 -0600

    add RandomColor directive and ReversePipe; enhance HomeComponent template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './ui/welcome.component';
+import { RandomColor } from './ui/random-color.directive';
+import { ReversePipe } from './ui/reverse.pipe';
 
 @Component({
     selector: 'app-home',
     template: `
         <app-welcome /> 
         <p>I am the home component</p>
+        <p randomColor>I am stylish</p>
+        <p>Time for a little: {{ magic | reverse }}</p>
  `,
-    imports: [WelcomeComponent],
+    imports: [WelcomeComponent, RandomColor, ReversePipe],
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    magic = 'reverse me';
+}
